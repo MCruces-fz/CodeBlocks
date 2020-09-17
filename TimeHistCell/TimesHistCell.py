05dbf4c0a686d892cea0e0085fe56c4768f02f7f
MCruces-fz <mcsquared.fz@gmail.com>
2020-09-17 12:46:21 +0200

    Computed cluster matrices and upgraded code.

--- TimeHistCell/TimesHistCell.py
+++ TimeHistCell/TimesHistCell.py
@@ -5,7 +5,8 @@
 @author: Sara Costa Faya
 
 EDIT: Miguel Cruces
-M-AILs:
+
+E-MAILs:
   - mcsquared.fz@gmail.com
   - miguel.cruces.fernandez@usc.es
 """
@@ -16,12 +17,14 @@ from os.path import join as join_path
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+import time as tm
 
 
 class CookingData:
     """
     Class which prepares and plots histograms
     """
+
     def __init__(self):
         # Root Directory of the Project
         self.root_dir = os.path.abspath("./")
@@ -30,11 +33,36 @@ class CookingData:
         if self.root_dir not in sys.path:
             sys.path.append(self.root_dir)
 
-        # Read data
-        data_path = join_path(self.root_dir, "dst_export_sara_3.txt")
-        data = np.loadtxt(data_path, delimiter=',', usecols=range(186))  # , max_rows=500)
+        self.coord_ix = {1: [1, 1], 2: [2, 1], 3: [3, 1], 4: [4, 1], 5: [5, 1], 6: [6, 1],
+                         7: [1, 2], 8: [2, 2], 9: [3, 2], 10: [4, 2], 11: [5, 2], 12: [6, 2],
+                         13: [1, 3], 14: [2, 3], 15: [3, 3], 16: [4, 3], 17: [5, 3], 18: [6, 3],
+                         19: [1, 4], 20: [2, 4], 21: [3, 4], 22: [4, 4], 23: [5, 4], 24: [6, 4],
+                         25: [1, 5], 26: [2, 5], 27: [3, 5], 28: [4, 5], 29: [5, 5], 30: [6, 5]}
 
-        self.mdat = self.set_mdat(data)
+        time_0 = tm.time()
+
+        # Read data
+        data_path = join_path(self.root_dir, "dst_export.txt")
+        data = np.loadtxt(data_path, delimiter=',')  # , max_rows=500)  # , usecols=range(186))
+
+        time_1 = tm.time()
+        print(f"Time reading data: {time_1 - time_0:.3f} seconds")
+
+        self.mdat = None
+        self.set_mdat(data)
+
+        self.time_clusters = None
+        self.char_clusters = None
+        self.set_matrices(data)
+
+        time_2 = tm.time()
+        print(f"Time setting matrices: {time_2 - time_1:.3f} seconds")
+
+        self.make_histograms()
+
+        time_3 = tm.time()
+        print(f"Time making histograms: {time_3 - time_2:.3f} seconds")
+        print(f"Total time: {time_3 - time_0:.3f} seconds")
 
     def set_mdat(self, data, multi=None):
         """
@@ -56,17 +84,13 @@ class CookingData:
         iq2cel = 96
         iq3cel = 156
 
-        coord_ix = {1: [1, 1], 2: [2, 1], 3: [3, 1], 4: [4, 1], 5: [5, 1], 6: [6, 1],
-                    7: [1, 2], 8: [2, 2], 9: [3, 2], 10: [4, 2], 11: [5, 2], 12: [6, 2],
-                    13: [1, 3], 14: [2, 3], 15: [3, 3], 16: [4, 3], 17: [5, 3], 18: [6, 3],
-                    19: [1, 4], 20: [2, 4], 21: [3, 4], 22: [4, 4], 23: [5, 4], 24: [6, 4],
-                    25: [1, 5], 26: [2, 5], 27: [3, 5], 28: [4, 5], 29: [5, 5], 30: [6, 5]}
-
         m_dat = np.zeros([0, 4 * len(multi)])
         for row in data:
-            # Arrays with indexes of non-zero values
+            # Arrays with values of temperatures and charges of planes 1, 2 and 3 respectively
             t1, t2, t3 = row[it1cel:it1cel + 30], row[it2cel:it2cel + 30], row[it3cel:it3cel + 30]
             q1, q2, q3 = row[iq1cel:iq1cel + 30], row[iq2cel:iq2cel + 30], row[iq3cel:iq3cel + 30]
+
+            # Arrays with indexes of non-zero values
             t1_id = np.nonzero(t1)[0]
             t2_id = np.nonzero(t2)[0]
             t3_id = np.nonzero(t3)[0]
@@ -74,24 +98,69 @@ class CookingData:
             q2_id = np.nonzero(q2)[0]
             q3_id = np.nonzero(q3)[0]
 
+            # MDAT
             multi_list = [len(t1_id), len(t2_id), len(t3_id), len(q1_id), len(q2_id), len(q3_id)]
 
             # Only hits with multiplicity "multi" on time and charge (multi*2) will be stored on m_dat
             if multi_list == multi * 2:
-                kx1, ky1 = coord_ix[t1_id[0] + 1]
-                kx2, ky2 = coord_ix[t2_id[0] + 1]
-                kx3, ky3 = coord_ix[t3_id[0] + 1]
+                kx1, ky1 = self.coord_ix[t1_id[0] + 1]
+                kx2, ky2 = self.coord_ix[t2_id[0] + 1]
+                kx3, ky3 = self.coord_ix[t3_id[0] + 1]
 
                 new_row = np.hstack((kx1, ky1, t1[t1_id[0]], q1[q1_id[0]],
                                      kx2, ky2, t2[t2_id[0]], q2[q2_id[0]],
                                      kx3, ky3, t3[t3_id[0]], q3[q3_id[0]]))
                 m_dat = np.vstack((m_dat, new_row))
-        return m_dat
+
+        self.mdat = m_dat
+
+    def set_matrices(self, data):
+        time_clusters = empty_list([3, 5])
+        char_clusters = empty_list([3, 5])
+        for row in data:
+            # Arrays with values of temperatures and charges of planes 1, 2 and 3 respectively
+            times_list = [row[it:it + 30] for it in range(6, 127, 60)]
+            chars_list = [row[iq:iq + 30] for iq in range(36, 157, 60)]
+
+            # Arrays with indexes of non-zero values
+            times_id_list = [np.nonzero(tim)[0] for tim in times_list]
+            chars_id_list = [np.nonzero(cha)[0] for cha in chars_list]
+
+            # MULTIPLICITY MATRICES [CLUSTERS]
+
+            for p in range(len(times_id_list)):  # Planes
+                multiplicities = len(times_id_list[p])
+                if multiplicities == 0 or multiplicities > 5:
+                    continue
+                ary = [self.coord_ix[k + 1] for k in times_id_list[p]]
+                coords = np.hstack(ary)
+                # common_ix = np.intersect1d(t1_id, q1_id)  # Con esto tenemos un array con los valores comunes
+                time_clusters[p][multiplicities - 1].append(np.hstack((coords, times_list[p][times_id_list[p]])))
+
+            for p in range(len(chars_id_list)):  # Planes
+                multiplicities = len(chars_id_list[p])
+                if multiplicities == 0 or multiplicities > 5:
+                    continue
+                ary = [self.coord_ix[k + 1] for k in chars_id_list[p]]
+                coords = np.hstack(ary)
+                # common_ix = np.intersect1d(t1_id, q1_id)  # Con esto tenemos un array con los valores comunes
+                char_clusters[p][multiplicities - 1].append(np.hstack((coords, chars_list[p][chars_id_list[p]])))
+
+        for p in range(len(time_clusters)):  # Planes
+            for m in range(len(time_clusters[p])):  # Multiplicities
+                time_clusters[p][m] = np.vstack(time_clusters[p][m])
+
+        for p in range(len(char_clusters)):  # Planes
+            for m in range(len(char_clusters[p])):  # Multiplicities
+                char_clusters[p][m] = np.vstack(char_clusters[p][m])
+        self.time_clusters = time_clusters
+        self.char_clusters = char_clusters
 
     def make_histograms(self):
         """
         This function sorts all data and creates 180 histograms for
-        time and charge, one for each cell in each plane.
+        time and charge, one for each cell in each plane. They are
+        stored on ROOT_DIR/Histograms/
         :return: It is a void function.
         """
         # 3D array of shape (No. Planes, No. X cells, No. Y cells)
@@ -106,33 +175,36 @@ class CookingData:
                 time_hist[p][x - 1][y - 1].append(time)
                 char_hist[p][x - 1][y - 1].append(charge)
 
+        histograms_dir = join_path(self.root_dir, "Histograms")
+        if not os.path.exists(histograms_dir):
+            os.mkdir(histograms_dir)
+
         # Time Histograms
-        for p in range(len(time_hist)):
-            for x in range(len(time_hist[p])):
-                for y in range(len(time_hist[p][x])):
-                    plt.figure(f"T_p{p + 1}x{x + 1}y{y + 1}")
-                    plt.title(f"Plane {p + 1} - Cell ({x + 1}, {y + 1}) - Time")
-                    arr = np.asarray(time_hist[p][x][y])
-                    filtered = arr[~is_outlier(arr)]
-                    plt.hist(filtered, bins="auto")
-                    plt.xlabel(f"time (T{p + 1})")
-                    plt.ylabel(f"# Counts")
-                    plt.savefig(f"./histograms/time_T{p + 1}_{x + 1}{y + 1}.png")
-                    plt.close(f"T_p{p + 1}x{x + 1}y{y + 1}")
+        plot_hist(time_hist, kind="time")
 
         # Charge Histograms
-        for p in range(len(char_hist)):
-            for x in range(len(char_hist[p])):
-                for y in range(len(char_hist[p][x])):
-                    plt.figure(f"Q_p{p + 1}x{x + 1}y{y + 1}")
-                    plt.title(f"Plane {p + 1} - Cell ({x + 1}, {y + 1}) - Charge")
-                    arr = np.asarray(char_hist[p][x][y])
-                    filtered = arr[~is_outlier(arr)]
-                    plt.hist(filtered, bins="auto")
-                    plt.xlabel(f"charge (T{p + 1})")
-                    plt.ylabel(f"# Counts")
-                    plt.savefig(f"./histograms/charge_T{p + 1}_{x + 1}{y + 1}.png")
-                    plt.close(f"Q_p{p + 1}x{x + 1}y{y + 1}")
+        plot_hist(char_hist, kind="charge")
+
+
+def plot_hist(data_hist, kind: str):
+    """
+    This function save all histograms on "./Histograms"
+    :param data_hist: list of lists with all data
+    :param kind: string ["time", "charge"]
+    :return: It is a void function
+    """
+    for p in range(len(data_hist)):
+        for x in range(len(data_hist[p])):
+            for y in range(len(data_hist[p][x])):
+                plt.figure(f"{kind}_p{p + 1}x{x + 1}y{y + 1}")
+                plt.title(f"Plane {p + 1} - Cell ({x + 1}, {y + 1}) - {kind}")
+                arr = np.asarray(data_hist[p][x][y])
+                filtered = arr[~is_outlier(arr)]
+                plt.hist(filtered, bins="auto")
+                plt.xlabel(f"{kind} (T{p + 1})")
+                plt.ylabel(f"# Counts")
+                plt.savefig(f"./Histograms/{kind}_T{p + 1}_{x + 1}{y + 1}.png")
+                plt.close(f"{kind}_p{p + 1}x{x + 1}y{y + 1}")
 
 
 def empty_list(shape):
@@ -142,31 +214,23 @@ def empty_list(shape):
     :return: empty list of empty lists of empty lists...
     """
     if len(shape) == 1:
-        return [[] for i in range(shape[0])]
+        return [[] for _ in range(shape[0])]
     items = shape[0]
     newshape = shape[1:]
     sublist = empty_list(newshape)
-    return [copy.deepcopy(sublist) for i in range(items)]
+    return [copy.deepcopy(sublist) for _ in range(items)]
 
 
 def is_outlier(points, thresh=3.5):
     """
     Returns a boolean array with True if points are outliers and False
     otherwise.
-
-    Parameters:
-    -----------
-        points : An numobservations by numdimensions array of observations
-        thresh : The modified z-score to use as a threshold. Observations with
+    :param points: An numobservations by numdimensions array of observations
+    :param thresh: The modified z-score to use as a threshold. Observations with
             a modified z-score (based on the median absolute deviation) greater
             than this value will be classified as outliers.
-
-    Returns:
-    --------
-        mask : A numobservations-length boolean array.
-
-    References:
-    ----------
+    :return: (mask) A numobservations-length boolean array.
+    :references:
         Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
         Handle Outliers", The ASQC Basic References in Quality Control:
         Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
@@ -186,4 +250,7 @@ def is_outlier(points, thresh=3.5):
 if __name__ == "__main__":
     CD = CookingData()
     mdat = CD.mdat
-    CD.make_histograms()
+
+    # k_clusters = list: [Plane][Multiplicity] -> [array]
+    t_clusters = CD.time_clusters
+    c_clusters = CD.char_clusters
